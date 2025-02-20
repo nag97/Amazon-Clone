37e99fecf2d28a0707ccbddcd3ccf8b1b6a69254
nag97 <nagarajcheera18@gmail.com>
2025-02-20 19:09:54 +0530

    Use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,4 @@
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    ratings: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    ratings: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    ratings: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
-
-let productsHTML = '';
-
+let productsHTML = "";
 
 products.forEach((product) => {
   productsHTML += `
@@ -45,14 +14,14 @@ products.forEach((product) => {
 
             <div class="product-rating-container">
               <img class="product-rating-stars"
-                src="images/ratings/rating-${product.ratings.stars * 10}.png">
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
               <div class="product-rating-count link-primary">
-                ${product.ratings.count}
+                ${product.rating.count}
               </div>
             </div>
 
             <div class="product-price">
-              $${(product.priceCents  / 100).toFixed(2)}
+              $${(product.priceCents / 100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
@@ -82,8 +51,7 @@ products.forEach((product) => {
             </button>
           </div>
       `;
-      console.log(productsHTML);
+  console.log(productsHTML);
 });
 
-
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
