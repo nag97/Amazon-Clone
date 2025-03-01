4f234772f582f0560e6ded9201ce0f17dd260148
nag97 <nagarajcheera18@gmail.com>
2025-03-01 19:59:48 +0530

    Fixed .gitignore and removed untracked files

---
+++ Amazon-Clone/data/cart.js
@@ -0,0 +1,36 @@
+export const cart = [{
+  productId : 
+  `e43638ce-6aa0-4b85-b27f-e1d07eb678c6`,
+  quantity : 2,
+}, {
+  productId : `15b6fc6f-327a-4ec4-896f-486349e85a3d`,
+  quantity : 1,
+}
+];
+
+export function addToCart(productId){
+  let matchingItem;
+
+    cart.forEach((cartItem) => {
+      console.log(cartItem.productId);
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    // Get selected quantity from dropdown
+    let selectorQuantity = Number(
+      document.querySelector(`.js-quantity-selector-${productId}`).value, 
+      10
+    );
+
+    if (matchingItem) {
+      matchingItem.quantity += selectorQuantity; 
+      
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: selectorQuantity 
+      });
+    }
+};
